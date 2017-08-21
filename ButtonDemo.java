360d831bc3b828b20af10bea6f426d27278a157a
rulusoy@studygroup.com <rulusoy@studygroup.com>
2017-08-21 10:36:31 +1000

    added a comment
    added a comment

--- ButtonDemo.java
+++ ButtonDemo.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
+
 public class ButtonDemo extends Application {
   protected Text text = new Text(50, 50, "JavaFX Programming");
   
